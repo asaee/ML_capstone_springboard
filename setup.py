0a54bf2b47d20607d904b08579f2af553d1ab104
Asaee <rasoul.asaee@NRCan-RNCan.gc.ca>
2021-08-24 21:03:59 -0400

    The first commit for the ML capstone project. This commit creates the directory structure for the project.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A NLP project to investigate the non-medical aspects of COVID-19.',
+    author='RasoulAsaee',
+    license='MIT',
+)
